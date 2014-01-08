5a16bfffe8959f8786295f000b0236c9dc1183b2
William Huang <wwhuang@umich.edu>
2014-01-08 18:06:59 -0500

    initial commit for SST25VF064C flash chip drivers

---
+++ tinyos/tos/platforms/opo2/chips/SST25VF/HplSST25VF064.h
@@ -0,0 +1,24 @@
+#define READ                  0x03
+#define FAST_READ_DUAL_IO     0xbb
+#define FAST_READ_DUAL_OUTPUT 0x3b
+#define HIGH_SPEED_READ       0x0b
+#define SECTOR_ERASE          0x20
+#define 32KB_BLOCK_ERASE      0x52
+#define 64KB_BLOCK_ERASE      0xD8
+#define CHIP_ERASE            0x60
+#define CHIP_ERASE_2          0xc7
+#define PAGE_PROGRAM          0x02
+#define DUAL_IN_PAGE_PROGRAM  0xa2
+#define RDSR                  0x05
+#define EWSR                  0x50
+#define WRSR                  0x01
+#define WREN                  0x06
+#define WRDI                  0x04
+#define RDID                  0x90
+#define RDID_2                0xab
+#define JEDEC_ID              0x9f
+#define EHLD                  0xaa
+#define READ_SID              0x88
+#define PROGRAM_SID           0xa5
+#define LOCKOUT_SID           0x85
+
---
+++ tinyos/tos/platforms/opo2/chips/SST25VF/HplSST25VF064.nc
@@ -0,0 +1,3 @@
+interface HplSST25VF064 {
+
+}
