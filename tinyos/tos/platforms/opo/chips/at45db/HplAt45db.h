6e82713316e9d4abb29469f25ce3651a29a22cf3
William Huang <wwhuang@umich.edu>
2014-02-15 00:31:04 -0500

    from the beginning

---
+++ tinyos/tos/platforms/opo/chips/at45db/HplAt45db.h
@@ -0,0 +1,11 @@
+#ifndef HPL_AT45DB_H
+#define HPL_AT45DB_H
+
+#define BUFFER_WRITE_1 0x84
+#define BUFFER_WRITE_2 0x87
+#define BUFFER_TO_MEM_ERASE_1 0x83
+#define BUFFER_TO_MEM_ERASE_2 0x86
+#define CHIP_ERASE 0xc7
+#define CONTINUOUS_ARRAY_READ_LOW_FREQ 0x03
+
+#endif
