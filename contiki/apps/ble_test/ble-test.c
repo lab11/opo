2c4cf97de23deebedc648842dfe95d9561242962
William Huang <wwhuang@umich.edu>
2014-07-17 00:14:01 -0400

    adding in variables for nrf8001_init func

--- contiki/apps/ble_test/ble-test.c
+++ contiki/apps/ble_test/ble-test.c
@@ -11,15 +11,19 @@ PROCESS(ble_test, "BleTest");
 AUTOSTART_PROCESSES(&ble_test);
 /*---------------------------------------------------------------------------*/
 
+nrf8001_event_packet ep;
+
 PROCESS_THREAD(ble_test, ev, data) {
-	int i;
-
 	PROCESS_BEGIN();
-
-	leds_off(LEDS_ALL);
-
+	nrf8001_init(&ep);
+	leds_on(LEDS_ALL);
 	while(1) {
 		PROCESS_YIELD();
+		if (ev == sensors_event) {
+			if (data == &nrf8001_event) {
+				leds_on(LEDS_ALL);
+			}
+		}
 	}
 
 	PROCESS_END();
