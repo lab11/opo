0a1107906892e1bc94051f77a8b124e468948f7f
William Huang <wwhuang@umich.edu>
2014-07-18 01:26:56 -0400

    minor changes. App currently tests for device started event

--- contiki/apps/ble_test/ble-test.c
+++ contiki/apps/ble_test/ble-test.c
@@ -5,6 +5,7 @@
 #include <string.h>
 #include <stdio.h>
 #include "dev/leds.h"
+#include "nrf8001.h"
 
 /*---------------------------------------------------------------------------*/
 PROCESS(ble_test, "BleTest");
@@ -15,13 +16,22 @@ nrf8001_event_packet ep;
 
 PROCESS_THREAD(ble_test, ev, data) {
 	PROCESS_BEGIN();
-	nrf8001_init(&ep);
-	leds_on(LEDS_ALL);
+	nrf8001_enable();
 	while(1) {
 		PROCESS_YIELD();
-		if (ev == sensors_event) {
-			if (data == &nrf8001_event) {
-				leds_on(LEDS_ALL);
+		if(ev == sensors_event) {
+			uint8_t i = 0;
+			if(data == &nrf8001_event) {
+				leds_on(LEDS_GREEN);
+				ep = nrf8001_get_event();
+				printf("Length: %h\n", ep.length);
+				printf("Event: %h\n", ep.event);
+				for(i=0;i<ep.length-1;i++) {
+					printf("Packet %u: %h", ep.packet[i]);
+				}
+				if(ep.event == 0x81) {
+					leds_on(LEDS_RED);
+				}
 			}
 		}
 	}
