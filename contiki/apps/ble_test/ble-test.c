7d7964f628689a1064e77eb37a2f6a6d32af902b
William Huang <wwhuang@umich.edu>
2014-07-21 22:48:09 -0400

    added leds in for showing status of test program

--- contiki/apps/ble_test/ble-test.c
+++ contiki/apps/ble_test/ble-test.c
@@ -1,18 +1,30 @@
 #include "ble-test.h"
 #include "nrf8001.h"
 #include "contiki.h"
-#include <stdint.h>
-#include <string.h>
-#include <stdio.h>
 #include "dev/leds.h"
 #include "nrf8001.h"
+#include "dev/gpio.h"
+#include "dev/ioc.h"
 
 /*---------------------------------------------------------------------------*/
 PROCESS(ble_test, "BleTest");
 AUTOSTART_PROCESSES(&ble_test);
-/*---------------------------------------------------------------------------*/
+/*---------------------s------------------------------------------------------*/
 
-nrf8001_event_packet ep;
+nrf8001_event_packet ep = {0};
+
+// Function to delay startup until we pull a pin high
+static void config_delay_interrupt() {
+	GPIO_SOFTWARE_CONTROL(DELAY_INT_PORT_BASE, DELAY_INT_PIN_MASK);
+	GPIO_SET_INPUT(DELAY_INT_PORT_BASE, DELAY_INT_PIN_MASK);
+	GPIO_DETECT_EDGE(DELAY_INT_PORT_BASE, DELAY_INT_PIN_MASK);
+	GPIO_TRIGGER_SINGLE_EDGE(DELAY_INT_PORT_BASE, DELAY_INT_PIN_MASK);
+	GPIO_DETECT_FALLING(DELAY_INT_PORT_BASE, DELAY_INT_PIN_MASK);
+	GPIO_ENABLE_INTERRUPT(DELAY_INT_PORT_BASE, DELAY_INT_PIN_MASK);
+	ioc_set_over(DELAY_INT_PORT, DELAY_INT_PIN, IOC_OVERRIDE_PUE);
+	nvic_interrupt_enable(DELAY_INT_PORT);
+	gpio_register_callback(nrf8001_enable, DELAY_INT_PORT, DELAY_INT_PIN);
+}
 
 PROCESS_THREAD(ble_test, ev, data) {
 	PROCESS_BEGIN();
@@ -21,6 +33,7 @@ PROCESS_THREAD(ble_test, ev, data) {
 		PROCESS_YIELD();
 		if(ev == sensors_event) {
 			uint8_t i = 0;
+			ep = nrf8001_get_event();
 			if(data == &nrf8001_event) {
 				if(ep.event == 0x81) {
 					uint8_t p[10];
@@ -28,16 +41,16 @@ PROCESS_THREAD(ble_test, ev, data) {
 						p[i] = i;
 					}
 					if(ep.packet[0] == 0x02) {
-						leds_on(LEDS_GREEN);
+						leds_on(LEDS_RED);
 						nrf8001_test(0x02);
 					}
 					else if(ep.packet[0] == 0x01) {
-						leds_on(LEDS_RED);
+						leds_on(LEDS_YELLOW);
 						nrf8001_echo(10, &p);
 					}
 				}
 				else if (ep.event == 0x82) {
-					leds_on(LEDS_YELLOW);
+					leds_on(LEDS_GREEN);
 				}
 			}
 		}
