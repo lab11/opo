6309a1718d87b6d3291ba7ac41ad4bdccd032a1d
William Huang <wwhuang@umich.edu>
2015-01-30 15:52:03 -0500

    tests to see if phone can receive data from cloudcomm interface

--- contiki/apps/cloudcomm_test/cloudcomm-test.c
+++ contiki/apps/cloudcomm_test/cloudcomm-test.c
@@ -1,14 +1,17 @@
 #include "cloudcomm-test.h"
 #include "dev/leds.h"
-
+#include <stdlib.h>
+#include "nrf8001.h"
 
 PROCESS(cloudcomm_test, "CCTest");
 AUTOSTART_PROCESSES(&cloudcomm_test);
-
+static uint8_t meh[4] = {74, 65, 78, 0};
 
 PROCESS_THREAD(cloudcomm_test, ev, data) {
 	// lets us know that we can keep transmitting
 	PROCESS_BEGIN();
-	leds_on(LEDS_RED);
+	nrf8001_reset();
+	start_cloudcomm();
+	send_cloudcomm_data(&meh, 4);
 	PROCESS_END();
 }
