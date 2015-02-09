631c203dc0804297e1f31d0b78f1d1b47e205a1c
William Huang <wwhuang@umich.edu>
2015-02-09 09:57:18 -0500

    removed a bunch of stuff that is taken care of in other files

--- contiki/apps/power_test_null/null-power-test.c
+++ contiki/apps/power_test_null/null-power-test.c
@@ -1,15 +1,11 @@
 #include <stdlib.h>
 #include "contiki.h"
-#include "dev/systick.h"
-#include "rv4162.h"
+
 
 PROCESS(power_test, "powerTest");
 AUTOSTART_PROCESSES(&power_test);
 
 PROCESS_THREAD(power_test, ev, data) {
 	PROCESS_BEGIN();
-	rv4162_disable_clkout();
-	REG(SYSTICK_STCTRL) &= ~SYSTICK_STCTRL_INTEN;
-
 	PROCESS_END();
 }
--- contiki/apps/power_test_null/project-conf.h
+++ contiki/apps/power_test_null/project-conf.h
@@ -1,9 +1,4 @@
 #ifndef PROJECT_CONF_H_
 #define PROJECT_CONF_H_
 
-#define NETSTACK_CONF_RDC	nullrdc_driver
-#define NETSTACK_CONF_MAC	csma_driver
-#define NETSTACK_CONF_NETWORK	simple_network_driver
-#define CC2538_RF_CONF_CHANNEL 21
-
 #endif /* PROJECT_CONF_H_ */
