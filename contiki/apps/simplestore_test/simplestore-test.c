28765740890f9cd38117da576f1aca65a6efb832
William Huang <wwhuang@umich.edu>
2018-03-06 16:51:57 -0800

    updated for latest opo platform

--- contiki/apps/simplestore_test/simplestore-test.c
+++ contiki/apps/simplestore_test/simplestore-test.c
@@ -9,6 +9,9 @@
 #include "simplestore.h"
 #include "vtimer.h"
 #include "opo.h"
+#include "cc2538-rf-debug.h"
+#include "sst25vf.h"
+
 
 #define CC2538_RF_CONF_CHANNEL 21
 
@@ -27,16 +30,39 @@ PROCESS_THREAD(flash_test, ev, data) {
 	PROCESS_BEGIN();
 	uint8_t i = 0;
 	bool tester = true;
+  char buffer[100];
 	for(i=0;i<120;i++) {
-		wbuf[i] = i;
+		wbuf[i] = i+3;
     rbuf[i] = 0;
 	}
 	vt = get_vtimer(vtcallback);
+
+  simplestore_turn_off_flash();
+  simplestore_turn_on_flash(); 
+
+  /* 
+  sst25vf_clear_all_block_protection();
+
+  uint8_t bp_buf[20];
+  sst25vf_read_block_protection_register(bp_buf);
+
+  uint8_t ptr = 0;
+  for( i = 0; i < 18; i++)
+  {
+    ptr += snprintf(buffer + ptr, sizeof(buffer) - ptr, "%u ", bp_buf[i]);   
+  }
+  send_rf_debug_msg(buffer);
+  */
   simplestore_clear_flash_chip();
 
-  uint8_t w = simplestore_write_next_page(&wbuf[0], 120);
-  uint8_t r = simplestore_read_next_page(&rbuf[0], 120);
+  uint8_t w = simplestore_write_next_page(wbuf, 120);
+  uint8_t r = simplestore_read_next_page(rbuf, 120);
 
+  
+  /*for(i=0;i<120;i++) {
+    snprintf(buffer, 100, "SS_TEST RX: %u", rbuf[i]);
+    send_rf_debug_msg(buffer);
+  }*/ 
   for(i=0;i<120;i++) {
   	if(wbuf[i] != rbuf[i]) {
   		tester = false;
@@ -46,9 +72,11 @@ PROCESS_THREAD(flash_test, ev, data) {
 
   if(tester) {
   	leds_on(LEDS_GREEN);
+    leds_on(LEDS_BLUE);
   } else {
     leds_on(LEDS_RED);
   }
 
+  simplestore_clear_flash_chip();
 	PROCESS_END();
 }
