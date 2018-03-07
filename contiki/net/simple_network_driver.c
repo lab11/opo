1b1bf38a76622fa285d3e8c41e6f79e7046f4ab5
William Huang <wwhuang@umich.edu>
2018-03-06 17:06:26 -0800

    added some comments

--- contiki/net/simple_network_driver.c
+++ contiki/net/simple_network_driver.c
@@ -2,6 +2,14 @@
 #include "net/netstack.h"
 #include "dev/leds.h"
 
+/* 
+Platform conf defines NETSTACK_CONF_NETWORK to point to simple_network_driver. 
+NETSTACK_CONF_NETWORK gets defined into NETSTACK_NETWORK in contiki/core/net/netstakc.h
+For opo, we define nullmac to NETSTACK_CONF_MAC and nullrdc to NETSTACK_CONF_RDC 
+Okay, so in cc2538 rf core, the RX_TX ISR eventually calls NETSTACK_RDC.input, which calls 
+NETSTACK_MAC.input, which calls NETSTACK_NETWORK.input, which finally calls simple_network_driver. 
+*/ 
+
 static void default_callback() {}
 static void (*m_callback)() = default_callback;
 
